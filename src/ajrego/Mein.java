8015b7b54600396a6e3cc2eb0616aaa030dbdc4e
AJRego <rego.andre99@gmail.com>
2020-09-16 20:19:16 -0500

    main app

--- src/ajrego/Mein.java
+++ src/ajrego/Mein.java
@@ -5,17 +5,48 @@
  */
 package ajrego;
 
+import com.sun.javafx.application.LauncherImpl;
+import javafx.application.Application;
+import javafx.application.Preloader;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 /**
  *
  * @author AJ Rego
  */
-public class Mein {
+public class Mein extends Application{
+
+    public static final int COUNT_LIMIT = 150000;
+
+    @Override
+    public void init() throws Exception {
+        for (int i = 0; i < COUNT_LIMIT; i++) {
+            double progress = (100 * i) / COUNT_LIMIT;
+            LauncherImpl.notifyPreloader(this, new Preloader.ProgressNotification(progress));
+        }
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/ajrego/view/FXMLLogin.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     /**
+     * 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+       LauncherImpl.launchApplication(Mein.class, MyPreloader.class, args);
     }
     
 }
